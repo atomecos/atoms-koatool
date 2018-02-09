535e2685394f0996cf259f3c76e77f9a08898215
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-09 20:28:21 +0700

    FIX: async call middleware

--- src/lib/util/compose.http.context.ts
+++ src/lib/util/compose.http.context.ts
@@ -2,9 +2,9 @@ import * as Koa from "koa";
 import { HttpContext, HttpContextStore } from "atoms-httpcore";
 import { IProcessContext } from "atomservicescore";
 
-export const composeHttpContext = (ctx: any, next: () => void, processContext?: IProcessContext) => {
+export const composeHttpContext = async (ctx: any, next: () => void, processContext?: IProcessContext) => {
   ctx.process = processContext;
   ctx.store = new HttpContextStore();
 
-  next();
+  await next();
 };
