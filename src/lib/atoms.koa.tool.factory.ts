322f191f77f480991ad1d9797663165aebed5258
Panit Tuangsuwan <tpanitte@hotmail.com>
2018-02-17 08:17:17 +0700

    RELEASE: v.0.0.1-alpha.20

--- src/lib/atoms.koa.tool.factory.ts
+++ src/lib/atoms.koa.tool.factory.ts
@@ -1,6 +1,7 @@
 import * as Koa from "koa";
 import * as BodyParser from "koa-bodyparser";
 import { IProcessContext } from "atomservicescore";
+import { Composing } from "atoms-httpcore";
 import { AtomsKoaApplication } from "./core/atoms.koa.application";
 import { Util } from "./util";
 
@@ -10,16 +11,16 @@ export const AtomsKoaToolFactory = (toolsetsName: string, properties: any) => {
   application.getProcessContext = () => Util.getProcessContext(application);
   application.setProcessContext = (processContext: IProcessContext) => Util.setProcessContext(application, processContext);
 
-  application.compose = (composing: Function, ...args: any[]) =>
+  application.compose = (composing: Composing, ...args: any[]) =>
     (ctx: Koa.Context, next: () => void) => composing.apply(undefined, [ctx, ...args, next]);
 
-  application.useCompose = (composing: Function, ...args: any[]) => {
+  application.useCompose = (composing: Composing, ...args: any[]) => {
     const composed: any = application.compose(composing, ...args);
     application.use(composed);
   };
 
   application.use(BodyParser());
-  application.use((ctx: Koa.Context, next: () => void) => Util.composeHttpContext(ctx, next, application.getProcessContext()));
+  application.use((ctx: Koa.Context, next: () => Promise<void>) => Util.composeHttpContext(ctx, next, application.getProcessContext()));
 
   return application;
 };
